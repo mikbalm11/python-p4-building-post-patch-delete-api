23f507521e60509a2d06a2c01459ad57958af451
Mustafa Mehmedoglu <mikbalm@gmail.com>
2025-05-11 20:55:49 -0400

    code-along!

--- server/migrations/versions/a50080834d2e_initial_migration.py
+++ server/migrations/versions/a50080834d2e_initial_migration.py
@@ -1,8 +1,8 @@
-"""create tables games, reviews, users
+"""initial migration
 
-Revision ID: 57881204f4e6
+Revision ID: a50080834d2e
 Revises: 
-Create Date: 2022-09-12 11:38:12.948877
+Create Date: 2025-05-11 20:33:45.493666
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '57881204f4e6'
+revision = 'a50080834d2e'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -44,8 +44,8 @@ def upgrade():
     sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.Column('game_id', sa.Integer(), nullable=True),
     sa.Column('user_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['game_id'], ['games.id'], ),
-    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
+    sa.ForeignKeyConstraint(['game_id'], ['games.id'], name=op.f('fk_reviews_game_id_games')),
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], name=op.f('fk_reviews_user_id_users')),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
